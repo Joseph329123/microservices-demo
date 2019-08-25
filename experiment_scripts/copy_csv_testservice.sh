bfc559ef16b89439e04915517fa51ccd2b4c5afc
Joseph <josephworku1@gmail.com>
2019-08-25 17:55:09 -0400

    change output filename in testservice container

--- experiment_scripts/copy_csv_testservice.sh
+++ experiment_scripts/copy_csv_testservice.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# script to copy the csv (one line) from testservice container into local machine
+# script to copy the csv from testservice container into local machine
 
 test_service_name=$(kubectl get pods | grep testservice | awk '{print $1}')
 
-while [ ! $(kubectl exec $test_service_name -- ls | grep 'test_service_measurements.csv') ]
+while [ ! $(kubectl exec $test_service_name -- ls | grep 'results_unformatted.csv') ]
 do
-    echo "cannot find file: ${pod_with_csv}"
+    sleep 10s
 done
 
 # copy to local machine
-kubectl exec $test_service_name -- cat 'test_service_measurements.csv' >> output/test_service_measurements.csv
-printf "\n" >> output/test_service_measurements.csv
+kubectl exec $test_service_name -- cat 'results_unformatted.csv' >> output/results_unformatted.csv
