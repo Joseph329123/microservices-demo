3f7df271d909bf38e8b036aea23ee46a8b84139a
Joseph <josephworku1@gmail.com>
2019-08-25 17:57:26 -0400

    refactor experiment-scripts directory

--- experiment_scripts/orchestrate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./deploy_pods.sh
-./copy_csv.sh
-./destroy_pods.sh
--- experiment_scripts/touch_experiment_csv.sh
+++
@@ -1,7 +0,0 @@
-# generates the csv with its header (columns) inside ./output directory
-
-rm -f ./output/test_service_measurements.csv
-touch ./output/test_service_measurements.csv
-
-echo "service, best response time, worst response time, average response time, std dev response time, availability" >> ./output/test_service_measurements.csv 
-
